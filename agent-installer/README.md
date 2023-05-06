60180c4ced5d55a6fd29c9d833e957d909d226f0
pwt <1089749+pwt@users.noreply.github.com>
2023-05-06 10:51:17 +0100

    Streamline installer script for CWPs & externalise all vars

--- agent-installer/README.md
+++ agent-installer/README.md
@@ -1,15 +1,17 @@
 # YellowDog Agent Installer Script
 
-The Bash script [yd-agent-installer.sh](yd-agent-installer.sh) can be used to install and configure the YellowDog Agent and its dependencies on Linux instances using a number of different Linux distros.
+The Bash script [yd-agent-installer.sh](yd-agent-installer.sh) can be used to install and configure the YellowDog Agent and its dependencies on Linux instances running a number of different Linux distros.
 
-Before use, the variables below must be populated in the script, to allow download of the YellowDog Agent. Please contact YellowDog for the required credentials.
+Before use, the variables `NEXUS_USERNAME` and `NEXUS_PASSWORD` below must be provided, to allow download of the YellowDog Agent. Please contact YellowDog for the required credentials.
 
 ```shell
-# Uncomment and set the Nexus username and password below.
+# Set the Nexus username and password below or via environment.
 # These are required to download the YellowDog Agent JAR file.
-# NEXUS_USERNAME="<INSERT YELLOWDOG NEXUS USERNAME HERE>"
-# NEXUS_PASSWORD="<INSERT YELLOWDOG NEXUS PASSWORD HERE>"
+NEXUS_USERNAME="${NEXUS_USERNAME:-}"
+NEXUS_PASSWORD="${NEXUS_PASSWORD:-}"
 ```
+
+The credentials can be set in the environment or by directly editing the script.
 
 ## Script Details
 
@@ -34,7 +36,7 @@ The script is designed to work with recent Linux distributions based on **Debian
 - Red Hat Enterprise Linux 9.1
 - CentOS Stream 8 & 9
 - AlmaLinux 9.1
-- Amazon Linux 2 (but note that Amaxon Linux 2023 doesn't currently work with YellowDog)
+- Amazon Linux 2 (but note that Amaxon Linux 2023 doesn't currently work with YellowDog due to the requirement to use IMDSv2)
 - SUSE (SLES 15 SP4)
 
 ## Modes of Use
@@ -56,15 +58,30 @@ The user data file will be run (as root) when the instance boots, and will confi
 
 When using dynamic Agent installation, bear in mind that **every** provisioned instance will incur the costs of installing Java using the distro's package manager (probably using cloud-local repositories for the Linux distro you're using), and also of downloading the YellowDog Agent (about 35MB in size) from YellowDog's external Nexus repository. For these reasons, we recommend against using this approach when provisioning instances at scale: use a custom image instead.
 
-### Configured Worker Pool Creation
+### Configured Worker Pool Installation
 
 The installer script can also be used to install and configure the YellowDog Agent on systems that will be included in **Configured Worker Pools**.
 
-Configured Worker Pools are on-premise systems, or systems that were otherwise not provisioned via the YellowDog Scheduler, e.g., instances that were provisioned directly via a cloud provider console. Adding the YellowDog Agent to these systems allows them to participate in YellowDog Task scheduling.
+Configured Worker Pools are on-premise systems, or systems that were otherwise not provisioned via the YellowDog Scheduler, e.g., instances/hosts that were provisioned directly using on-premise provisioners or via a cloud provider console. Adding the YellowDog Agent to these systems allows them to participate in YellowDog Task scheduling.
 
-To use this feature:
+To use this feature, set the variable `CONFIGURED_WP` to `"TRUE"`. This will populate additional properties in the Agent's `application.yaml` configuration file (since these are not being set automatically as they are in the case of instances in Provisioned Worker Pools).
 
-1. Uncomment the section of the script starting with `CONFIGURED_WP="TRUE"`. This will populate additional properties in the Agent's `application.yaml` configuration file.
-2. The other `YD_` variables in this section can be populated directly in the script, or overridden via external environment variables of the same name.
-3. At a minimum, a **Configured Worker Pool Token** must be supplied in the `YD_TOKEN` variable. This tells the node to which Configured Worker Pool it belongs.
-4. The `YD_INSTANCE_ID` variable must be unique within the Worker Pool. By default, it's set to the hostname.
+The following variables are available for specifying the properties of an instance. These properties are advertised by the YellowDog Agent when it connects to the Worker Pool, and can be used for matching Task Groups to Workers. Note that Only one property, `YD_TOKEN` is required. These variables can be set/exported in the environment in which the installer script runs.
+
+| Property                 | Description                                                                                                                                         |
+|--------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------|
+| `YD_TOKEN` (Required)    | This is the token that identifies the Configured Worker Pool to which this instance belongs, and which allows the Agent to connect to the platform. |
+| `YD_INSTANCE_ID`         | An instance identifier, which must be unique within a Worker Pool. By default, the hostname found in `/etc/hostname` is used.                       |
+| `YD_HOSTNAME`            | The hostname of the instance. By default, the hostname found in `/etc/hostname` is used.                                                            |
+| `YD_REGION`              | A string describing the region in which the instance is located. Empty by default.                                                                  |
+| `YD_SOURCE_NAME`         | A string describing the 'source name' from which the instance comes, e.g.: "VMware 01". Empty by default.                                           |
+| `YD_INSTANCE_TYPE`       | A string describing the type of the instance. Empty by default.                                                                                     |
+| `YD_WORKER_TAG`          | A string that tags the worker(s), used for matching Task Groups to Workers. Empty by default.                                                       |
+| `YD_RAM`                 | The instance's RAM in GB. By default, the `MemTotal` value obtained from `/proc/meminfo`.                                                           |
+| `YD_VCPUS`               | The instance's VCPU count. By default, the value returned by `nproc`.                                                                               |
+| `YD_PUBLIC_IP`           | The instance's public IP address. Empty by default.                                                                                                 |
+| `YD_PRIVATE_IP`          | The instance's private IP address. Empty by default.                                                                                                |
+| `YD_WORKER_TARGET_COUNT` | The number of workers to create per Node or per vCPU (as determined by `YD_WORKER_TARGET_TYPE`). By default, `1`.                                   |
+| `YD_WORKER_TARGET_TYPE`  | Must be set to `"PER_NODE"` or `"PER_VCPU"`. By default, `"PER_NODE"`.                                                                              |
+| `YD_URL`                 | The URL of the YellowDog Platform's REST API. By default, `https://portal.yellowdog.co/api`.                                                        |
+| `YD_LOG_STR`             | Specifies the format of the Agent's `log4j` output. By default, `${YD_LOG_STR:-%d{yyyy-MM-dd HH:mm:ss,SSS} [%10.10thread] %-5level %message %n}`    |
--- agent-installer/yd-agent-installer.sh
+++ agent-installer/yd-agent-installer.sh
@@ -2,15 +2,24 @@
 
 # YellowDog Agent installer script.
 
-# Uncomment and set the Nexus username and password below.
+# Set the Nexus username and password below or via the environment.
 # These are required to download the YellowDog Agent JAR file.
-# NEXUS_USERNAME="<INSERT YELLOWDOG NEXUS USERNAME HERE>"
-# NEXUS_PASSWORD="<INSERT YELLOWDOG NEXUS PASSWORD HERE>"
+NEXUS_USERNAME="${NEXUS_USERNAME:-}"
+NEXUS_PASSWORD="${NEXUS_PASSWORD:-}"
 
-# Set the following to anything other than "TRUE" to suppress
+# Set "INSTALL_JAVA" to anything other than "TRUE" to suppress
 # Java installation. The Agent startup script will expect to find
 # a Java (v11+) runtime at: /usr/bin/java.
-INSTALL_JAVA="TRUE"
+INSTALL_JAVA="${INSTALL_JAVA:-TRUE}"
+
+# Set to "TRUE" for a Configured Worker Pool installation
+CONFIGURED_WP="${CONFIGURED_WP:-FALSE}"
+
+# Define user and directory names used by the Agent
+YD_AGENT_USER="${YD_AGENT_USER:-yd-agent}"
+YD_AGENT_ROOT="${YD_AGENT_ROOT:-/opt/yellowdog}"
+YD_AGENT_HOME="${YD_AGENT_HOME:-/opt/yellowdog/agent}"
+YD_AGENT_DATA="${YD_AGENT_DATA:-/var/opt/yellowdog/agent/data}"
 
 ################################################################################
 
@@ -21,53 +30,15 @@ yd_log () {
   echo -e "*** YD" "$(date -u "+%Y-%m-%d_%H%M%S_UTC"):" "$@"
 }
 
-# Ignore non-zero exit codes from grep
-safe_grep() { grep "$@" || test $? = 1; }
-
-################################################################################
-
-# Uncomment the section below for Configured Worker Pool installations.
-#    Ensure 'YD_TOKEN', at least, is populated.
-
-#CONFIGURED_WP="TRUE"
-#
-## The following can be overridden from the environment or populated directly
-#YD_TOKEN="${YD_TOKEN:-}"
-#YD_VCPUS="${YD_VCPUS:-$(nproc)}"
-#YD_RAM="${YD_RAM:-$(safe_grep MemTotal /proc/meminfo | \
-#           awk -v OFMT='%.2f' '{mem_gb = $2 / (1024*1024) ; print mem_gb}')}"
-#YD_INSTANCE_ID="${YD_INSTANCE_ID:-$(cat /etc/hostname)}"
-#YD_REGION="${YD_REGION:-}"
-#YD_INSTANCE_TYPE="${YD_INSTANCE_TYPE:-}"
-#YD_SOURCE_NAME="${YD_SOURCE_NAME:-}"
-#YD_PUBLIC_IP="${YD_PUBLIC_IP:-}"
-#YD_PRIVATE_IP="${YD_PRIVATE_IP:-}"
-#YD_HOSTNAME="${YD_HOSTNAME:-$(cat /etc/hostname)}"
-#YD_WORKER_TAG="${YD_WORKER_TAG:-}"
-#YD_WORKER_COUNT="${YD_WORKER_COUNT:-$YD_VCPUS}"
-#YD_URL="${YD_URL:-https://portal.yellowdog.co/api}"
-#YD_LOG_STR="${YD_LOG_STR:-%d{yyyy-MM-dd HH:mm:ss,SSS} [%10.10thread] \
-#%-5level %message %n}"
-
-################################################################################
+yd_log "Starting YellowDog Agent Setup"
 
 if [[ "$EUID" -ne 0 ]] ; then
   yd_log "Please run as root ... aborting"
   exit 1
 fi
 
-################################################################################
-
-# Define required variables
-YD_AGENT_USER="yd-agent"
-YD_AGENT_ROOT="/opt/yellowdog"
-YD_AGENT_HOME="/opt/yellowdog/agent"
-YD_AGENT_DATA="/var/opt/yellowdog/agent/data"
-MAVEN_REPO="maven-public"
-
-################################################################################
-
-yd_log "Starting YellowDog Agent Setup"
+# Ignore non-zero exit codes from grep
+safe_grep() { grep "$@" || test $? = 1; }
 
 ################################################################################
 
@@ -84,7 +55,7 @@ yd_log "Checking distro using 'ID_LIKE' from '/etc/os-release'"
 DISTRO=$(safe_grep "^ID_LIKE=" /etc/os-release | sed -e 's/ID_LIKE=//' \
          | sed -e 's/"//g' | awk '{print $1}')
 # If empty, use the 'ID' property
-if [[ "$DISTRO" == "" ]] ; then
+if [[ "$DISTRO" == "" ]]; then
   yd_log "Checking distro using 'ID' from '/etc/os-release'"
   DISTRO=$(safe_grep "^ID=" /etc/os-release | sed -e 's/ID=//' \
            | sed -e 's/"//g')
@@ -141,7 +112,7 @@ yd_log "Starting Agent download"
 
 BASIC_AUTH=$(printf '%s:%s' "$NEXUS_USERNAME" "$NEXUS_PASSWORD" | base64)
 curl --fail -Ls "https://nexus.yellowdog.tech/service/\
-rest/v1/search/assets/download?sort=version&repository=$MAVEN_REPO&maven.\
+rest/v1/search/assets/download?sort=version&repository=maven-public&maven.\
 groupId=co.yellowdog.platform&maven.artifactId=agent&maven.extension=jar" \
 -o "$YD_AGENT_HOME/agent.jar" -H "Authorization: Basic $BASIC_AUTH"
 
@@ -152,34 +123,36 @@ yd_log "Agent download complete"
 yd_log "Writing Agent configuration file (application.yaml)"
 yd_log "Inserting Task Type 'bash'"
 
-cat > $YD_AGENT_HOME/application.yaml <<- EOM
+cat > $YD_AGENT_HOME/application.yaml << EOM
 yda:
   taskTypes:
     - name: "bash"
       run: "/bin/bash"
 EOM
 
-CONFIGURED_WP="${CONFIGURED_WP:-FALSE}"
 if [[ $CONFIGURED_WP == "TRUE" ]]; then
+  # YD_TOKEN must be set for Configured Worker Pools
   yd_log "Adding Configured Worker Pool properties"
   cat >> $YD_AGENT_HOME/application.yaml << EOM
-  provider: "ON_PREMISE"
-  instanceId: "$YD_INSTANCE_ID"
-  hostname: "$YD_HOSTNAME"
   token: "$YD_TOKEN"
-  services-schema.default-url: "$YD_URL"
-  region: "$YD_REGION"
-  instanceType: "$YD_INSTANCE_TYPE"
-  sourceName: "$YD_SOURCE_NAME"
-  vcpus: $YD_VCPUS
-  ram: $YD_RAM
-  workerTag: "$YD_WORKER_TAG"
-  privateIpAddress: "$YD_PRIVATE_IP"
-  publicIpAddress: "$YD_PUBLIC_IP"
+  provider: "ON_PREMISE"
+  instanceId: "${YD_INSTANCE_ID:-$(cat /etc/hostname)}"
+  hostname: "${YD_HOSTNAME:-$(cat /etc/hostname)}"
+  services-schema.default-url: "${YD_URL:-https://portal.yellowdog.co/api}"
+  region: "${YD_REGION:-}"
+  instanceType: "${YD_INSTANCE_TYPE:-}"
+  sourceName: "${YD_SOURCE_NAME:-}"
+  vcpus: "${YD_VCPUS:-$(nproc)}"
+  ram: "${YD_RAM:-$(safe_grep MemTotal /proc/meminfo | \
+           awk -v OFMT='%.2f' '{mem_gb = $2 / (1024*1024) ; print mem_gb}')}"
+  workerTag: "${YD_WORKER_TAG:-}"
+  privateIpAddress: "${YD_PRIVATE_IP:-}"
+  publicIpAddress: "${YD_PUBLIC_IP:-}"
   createWorkers:
-    targetType: "PER_NODE"
-    targetCount: $YD_WORKER_COUNT
-  logging.pattern.console: "$YD_LOG_STR"
+    targetType: "${YD_WORKER_TARGET_TYPE:-PER_NODE}"
+    targetCount: "${YD_WORKER_TARGET_COUNT:1}"
+  logging.pattern.console: "${YD_LOG_STR:-%d{yyyy-MM-dd HH:mm:ss,SSS} \
+[%10.10thread] %-5level %message %n}"
 EOM
 fi
 
@@ -188,7 +161,7 @@ yd_log "Agent configuration file created"
 ################################################################################
 
 yd_log "Creating Agent startup script (start.sh)"
-cat > $YD_AGENT_HOME/start.sh <<- EOM
+cat > $YD_AGENT_HOME/start.sh << EOM
 #!/bin/sh
 /usr/bin/java -jar $YD_AGENT_HOME/agent.jar
 EOM
@@ -200,7 +173,7 @@ chmod ug+x $YD_AGENT_HOME/start.sh
 ################################################################################
 
 yd_log "Setting up the Agent systemd service"
-cat > /etc/systemd/system/yd-agent.service <<- EOM
+cat > /etc/systemd/system/yd-agent.service << EOM
 [Unit]
 Description=YellowDog Agent
 After=cloud-final.service
@@ -221,7 +194,7 @@ EOM
 
 mkdir -p /etc/systemd/system/yd-agent.service.d
 
-cat > /etc/systemd/system/yd-agent.service.d/yd-agent.conf <<- EOM
+cat > /etc/systemd/system/yd-agent.service.d/yd-agent.conf << EOM
 [Service]
 Environment="YD_AGENT_HOME=$YD_AGENT_HOME"
 Environment="YD_AGENT_DATA=$YD_AGENT_DATA"
@@ -248,15 +221,12 @@ yd_log "Agent service enabled and started"
 # Uncomment the following to add a public key for $YD_AGENT_USER
 
 #yd_log "Adding SSH public key for user $YD_AGENT_USER"
-#
 #mkdir -p $YD_AGENT_HOME/.ssh
 #chmod og-rwx $YD_AGENT_HOME/.ssh
-#
 ## Insert your public key below, between the two 'EOM' entries
 #cat >> $YD_AGENT_HOME/.ssh/authorized_keys << EOM
 #<Insert Public Key Here>
 #EOM
-#
 #chmod og-rw $YD_AGENT_HOME/.ssh/authorized_keys
 #chown -R $YD_AGENT_USER:$YD_AGENT_USER $YD_AGENT_HOME/.ssh
 
