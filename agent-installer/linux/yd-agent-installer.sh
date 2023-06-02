e5f058d3fec6fafb046d6655ca15318d78e5ee7e
pwt <1089749+pwt@users.noreply.github.com>
2023-06-02 14:47:48 +0100

    Add Agent schedule variables

--- agent-installer/linux/yd-agent-installer.sh
+++ agent-installer/linux/yd-agent-installer.sh
@@ -151,6 +151,12 @@ if [[ $YD_CONFIGURED_WP == "TRUE" ]]; then
   createWorkers:
     targetType: "${YD_WORKER_TARGET_TYPE:-PER_NODE}"
     targetCount: "${YD_WORKER_TARGET_COUNT:-1}"
+  schedule:
+    enabled: "${YD_SCHEDULE_ENABLED:-false}"
+    startup:
+      - "${YD_SCHEDULE_ENABLED_STARTUP:-0 0 18 * * MON-FRI}"
+    shutdown:
+      - "${YD_SCHEDULE_ENABLED_SHUTDOWN:-0 0 7 * * MON-FRI}"
 logging.pattern.console: "%d{yyyy-MM-ddHH:mm:ss,SSS} Worker[%10.10thread]\
  %-5level[%40logger{40}] %message [%class{0}:%method:%line]%n"
 EOM
